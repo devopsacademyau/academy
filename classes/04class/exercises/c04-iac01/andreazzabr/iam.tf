9c4ff48cf349c51d3e2f1b2cb4559e9f9e8988a5
anderson <andreazzabr@gmail.com>
2020-04-08 11:40:20 +1000

    andreazzabr/c04-iac01

---
+++ classes/04class/exercises/c04-iac01/andreazzabr/iam.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "roles3read" {
+    name = "roles3readonly"
+    assume_role_policy = <<-EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }
+    EOF
+}
+
+resource "aws_iam_role_policy" "rpols3read" {
+    name = "rpols3readonly"
+    role = aws_iam_role.roles3read.id
+    policy = <<-POLICY
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*"
+            ],
+            "Resource": "*"
+        }
+        ]
+    }
+ POLICY
+}
---
+++ classes/04class/exercises/c04-iac01/andreazzabr/security-group.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "SG_allow_ssh"
+  description = "Allow SSH traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
