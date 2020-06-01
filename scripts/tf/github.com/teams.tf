ce05006fd5dd6972e69ce76c3f1ef84b95e4bd29
Kiko <fcollet.silva@gmail.com>
2020-06-01 19:42:56 +1000

    adding new group with new term students (#631)

--- scripts/tf/github.com/teams.tf
+++ scripts/tf/github.com/teams.tf
@@ -92,3 +92,84 @@ module "team-students" {
     }
   ]
 }
+
+module "team-students-2020-jun" {
+  source              = "./da-teams"
+  github_token        = var.github_token
+  github_organization = var.github_organization
+  group_name          = "students-2020-jun"
+  members             = [
+    {
+      name: "alanlima",
+      role: "member"
+    },
+    {
+      name: "danielvca",
+      role: "member"
+    },
+    {
+      name: "fabioacp",
+      role: "member"
+    },
+    {
+      name: "feernandobraga",
+      role: "member"
+    },
+    {
+      name: "frdvo",
+      role: "member"
+    },
+    {
+      name: "gepifanio",
+      role: "member"
+    },
+    {
+      name: "gpavelar",
+      role: "member"
+    },
+    {
+      name: "igorjosesantos",
+      role: "member"
+    },
+    {
+      name: "jayanath",
+      role: "member"
+    },
+    {
+      name: "mail2kvanitha",
+      role: "member"
+    },
+    {
+      name: "mfreitassm",
+      role: "member"
+    },
+    {
+      name: "mrcsmonteiro",
+      role: "member"
+    },
+    {
+      name: "netizen89",
+      role: "member"
+    },
+    {
+      name: "obrientimothya",
+      role: "member"
+    },
+    {
+      name: "shahriar048",
+      role: "member"
+    },
+    {
+      name: "zarajoy",
+      role: "member"
+    }
+  ]
+}
+
+
+
+
+
+
+
+
