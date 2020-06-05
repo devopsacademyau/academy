f491a267200e410fb808ea5785c08a2ebe31728c
Francisco Collet <fcollet.silva@gmail.com>
2020-06-06 00:18:12 +1000

    removing troubleshooting steps

--- scripts/dashboard/run.sh
+++ scripts/dashboard/run.sh
@@ -3,6 +3,5 @@ ls -l /app
 cd /app/scripts/dashboard
 mkdir -p ~/.config/gspread/
 echo $GOOGLE_KEY |base64 -d > ~/.config/gspread/service_account.json
-cat ~/.config/gspread/service_account.json
 ./dashboard.sh
 python3 dashboard.py
