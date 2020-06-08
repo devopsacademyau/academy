777415c354320b24f392ac923011fb5e5f470ac7
Francisco Collet <fcollet.silva@gmail.com>
2020-06-08 13:25:20 +1000

    testing slack

--- scripts/dashboard/run.sh
+++ scripts/dashboard/run.sh
@@ -5,6 +5,6 @@ echo $GOOGLE_KEY |base64 -d > ~/.config/gspread/service_account.json
 ./dashboard.sh
 python3 dashboard.py
 
-curl -X POST -H 'Content-type: application/json' \
+  curl -X POST -H 'Content-type: application/json' \
   --data '{"text":"Hello, World!"}' \
-  https://hooks.slack.com/services/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
+  ${SLACK_WEKHOOK}
