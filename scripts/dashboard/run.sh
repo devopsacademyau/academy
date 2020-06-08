7205f1c6316b28fb17c848c177c7317278fa1593
Francisco Collet <fcollet.silva@gmail.com>
2020-06-08 13:17:53 +1000

    testing directly message

--- scripts/dashboard/run.sh
+++ scripts/dashboard/run.sh
@@ -4,3 +4,7 @@ mkdir -p ~/.config/gspread/
 echo $GOOGLE_KEY |base64 -d > ~/.config/gspread/service_account.json
 ./dashboard.sh
 python3 dashboard.py
+
+curl -X POST -H 'Content-type: application/json' \
+  --data '{"text":"Hello, World!"}' \
+  https://hooks.slack.com/services/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
