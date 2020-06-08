07b7bdc816e26bd0f4685bba82701ec088182f43
Francisco Collet <fcollet.silva@gmail.com>
2020-06-08 21:51:51 +1000

    testing

--- scripts/dashboard/publish.sh
+++ scripts/dashboard/publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir ~/.aws/
 echo "[default]" > ~/.aws/credentials
 echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
 echo "aws_secret_access_key = AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
