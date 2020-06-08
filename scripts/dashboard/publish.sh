fa01fecaf62e45f7153aa563699bf374a6fd4271
Francisco Collet <fcollet.silva@gmail.com>
2020-06-08 21:48:39 +1000

    testing

--- scripts/dashboard/publish.sh
+++ scripts/dashboard/publish.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
+echo "aws_secret_access_key = AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+
+
+echo "[default]" > ~/.aws/config
+echo "output = json" >> ~/.aws/config
+echo "region = ap-southeast-2" >> ~/.aws/config
+
 cd /app/scripts/dashboard
+
+
 
 aws s3 cp ./chart.png s3://devopsacademy.com.au/progression-chart/chart.png
 aws s3api put-object-acl --bucket devopsacademy.com.au --key progression-chart/chart/png --acl public-read
@@ -23,7 +34,7 @@ curl -X POST -H 'Content-type: application/json' \
         "text": "Updated Progression"
       },
       "block_id": "image4",
-      "image_url": "https://devopsacademy.com.au.s3-ap-southeast-2.amazonaws.com/progression-chart/chart.png",
+      "image_url": "https://devopsacademy.com.au/progression-chart/chart.png",
       "alt_text": "Labs Progression"
     }
   ]
