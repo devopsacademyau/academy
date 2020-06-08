8e3b6769c49716e9db7fd2cae9dd033f8e7069a8
Francisco Collet <fcollet.silva@gmail.com>
2020-06-08 13:53:56 +1000

    testing through automation

--- scripts/dashboard/run.sh
+++ scripts/dashboard/run.sh
@@ -5,6 +5,27 @@ echo $GOOGLE_KEY |base64 -d > ~/.config/gspread/service_account.json
 ./dashboard.sh
 python3 dashboard.py
 
-  curl -X POST -H 'Content-type: application/json' \
-  --data '{"text":"Hello, World!"}' \
-  ${SLACK_WEKHOOK}
+curl -X POST -H 'Content-type: application/json' \
+  ${SLACK_WEBHOOK} \
+  --data '{
+  "channel": "test-automation",
+  "blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*Updated Labs Progression! Lets get those lines to infinity and beyond!*"
+      }
+    },
+    {
+      "type": "image",
+      "title": {
+        "type": "plain_text",
+        "text": "Updated Progression"
+      },
+      "block_id": "image4",
+      "image_url": "http://placekitten.com/500/500",
+      "alt_text": "Labs Progression"
+    }
+  ]
+}'
