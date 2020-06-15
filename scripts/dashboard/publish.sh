7fc27e25368dfdcedbd273abce2b59ee7416c1f8
Francisco Collet <fcollet.silva@gmail.com>
2020-06-15 21:48:39 +1000

    removing troubleshoot lines

--- scripts/dashboard/publish.sh
+++ scripts/dashboard/publish.sh
@@ -2,7 +2,6 @@
 cd /app/scripts/dashboard
 
 export CHART_DATE=$(date +%Y-%m-%d)
-
 echo "Saving files to S3"
 aws s3 cp ./chart.png s3://devopsacademy.com.au/progression-chart/chart-${CHART_DATE}.png
 
@@ -11,8 +10,6 @@ sed -i "s,PUBLISH_URL,$PUBLISH_URL,g" slack_payload.json
 sed -i "s,SLACK_CHANNEL,$SLACK_CHANNEL,g" slack_payload.json
 sed -i "s,CHART_DATE,$CHART_DATE,g" slack_payload.json
 sed -i "s,PUBLISH_URL,$PUBLISH_URL,g"  slack_payload.json
-
-cat ./slack_payload.json
 
 echo "Publishing Slack message"
 curl -X POST -H 'Content-type: application/json' \
