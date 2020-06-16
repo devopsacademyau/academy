6bac345435a66dfaab0e64745c7145b31edee3d4
asantos <asantos@allette.com.au>
2020-06-16 18:04:12 +1000

    Add albertoeks as an external student member

---
+++ scripts/tf/github.com/team-external-students.tf
@@ -0,0 +1,28 @@
+module "team-external-students" {
+  source              = "./da-teams"
+  github_token        = var.github_token
+  github_organization = var.github_organization
+  group_name          = "external-students"
+  members             = [
+    {
+      name: "albertoeks",
+      role: "member"
+    },
+    {
+      name: "carloshz4",
+      role: "member"
+    },
+    {
+      name: "raghunadhpokkalath",
+      role: "member"
+    },
+    {
+      name: "rodrigovcesar",
+      role: "member"
+    },
+    {
+      name: "roperto",
+      role: "member"
+    }
+  ]
+}
