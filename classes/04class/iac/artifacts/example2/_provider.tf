0babe4bc2ee69f831f13e1ef9a90fbf51454abf2
Denis Silva <denisstortisilva@gmail.com>
2020-07-19 17:24:53 +1000

    Add images Add input assignment options Add data sources

--- classes/04class/iac/artifacts/example2/_provider.tf
+++ classes/04class/iac/artifacts/example2/_provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version = "~> 2.0"
   region  = "ap-southeast-2"
-	# export TF_VAR_profile=$AWS_PROFILE
-	profile = var.profile
+  # export TF_VAR_profile=$AWS_PROFILE
+  profile = var.profile
 }
--- classes/04class/iac/artifacts/example2/vpc.tf
+++ classes/04class/iac/artifacts/example2/vpc.tf
@@ -5,32 +5,32 @@ resource "aws_vpc" "devops_academy" {
   }
 }
 resource "aws_subnet" "subnet_private_a" {
-  vpc_id     = "${aws_vpc.devops_academy.id}"
-  cidr_block = "192.168.10.0/24"
+  vpc_id            = "${aws_vpc.devops_academy.id}"
+  cidr_block        = "192.168.10.0/24"
   availability_zone = "ap-southeast-2a"
   tags = {
     Name = "private-a"
   }
 }
 resource "aws_subnet" "subnet_private_b" {
-  vpc_id     = "${aws_vpc.devops_academy.id}"
-  cidr_block = "192.168.20.0/24"
+  vpc_id            = "${aws_vpc.devops_academy.id}"
+  cidr_block        = "192.168.20.0/24"
   availability_zone = "ap-southeast-2b"
   tags = {
     Name = "private-b"
   }
 }
 resource "aws_subnet" "subnet_public_a" {
-  vpc_id     = "${aws_vpc.devops_academy.id}"
-  cidr_block = "192.168.100.0/24"
+  vpc_id            = "${aws_vpc.devops_academy.id}"
+  cidr_block        = "192.168.100.0/24"
   availability_zone = "ap-southeast-2a"
   tags = {
     Name = "public-a"
   }
 }
 resource "aws_subnet" "subnet_public_b" {
-  vpc_id     = "${aws_vpc.devops_academy.id}"
-  cidr_block = "192.168.200.0/24"
+  vpc_id            = "${aws_vpc.devops_academy.id}"
+  cidr_block        = "192.168.200.0/24"
   availability_zone = "ap-southeast-2b"
   tags = {
     Name = "public-b"
@@ -62,32 +62,64 @@ resource "aws_route_table_association" "route_public_b" {
   route_table_id = "${aws_route_table.route_table_public.id}"
 }
 
-resource "aws_eip" "natgw_a" {
-  vpc = true
+# resource "aws_eip" "natgw_a" {
+#   vpc = true
+# }
+
+# resource "aws_nat_gateway" "gw_a" {
+#   allocation_id = "${aws_eip.natgw_a.id}"
+#   subnet_id     = "${aws_subnet.subnet_public_a.id}"
+#   tags = {
+#     Name = "NATGW-A"
+#   }
+# }
+
+# resource "aws_route_table" "route_table_private" {
+#   vpc_id = "${aws_vpc.devops_academy.id}"
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = "${aws_nat_gateway.gw_a.id}"
+#   }
+#   tags = {
+#     Name = "PrivateRoutingTable"
+#   }
+# }
+
+# resource "aws_route_table_association" "route_private_a" {
+#   subnet_id      = "${aws_subnet.subnet_private_a.id}"
+#   route_table_id = "${aws_route_table.route_table_private.id}"
+# }
+# resource "aws_route_table_association" "route_private_b" {
+#   subnet_id      = "${aws_subnet.subnet_private_b.id}"
+#   route_table_id = "${aws_route_table.route_table_private.id}"
+# }
+
+data "aws_ami" "example" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  tags = {
+    Name   = "app-server"
+    Tested = "true"
+  }
 }
 
-resource "aws_nat_gateway" "gw_a" {
-  allocation_id = "${aws_eip.natgw_a.id}"
-  subnet_id     = "${aws_subnet.subnet_public_a.id}"
+resource "aws_instance" "da_academy" {
+  ami           = data.aws_ami.example.id
+  subnet_id     = aws_subnet.subnet_public_b.id
+  instance_type = "t2.micro"
+
   tags = {
-    Name = "NATGW-A"
+    Name = "DevOpsAcademy-Instance"
   }
-}
-resource "aws_route_table" "route_table_private" {
-  vpc_id = "${aws_vpc.devops_academy.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_nat_gateway.gw_a.id}"
-  }
-  tags = {
-    Name = "PrivateRoutingTable"
-  }
-}
-resource "aws_route_table_association" "route_private_a" {
-  subnet_id      = "${aws_subnet.subnet_private_a.id}"
-  route_table_id = "${aws_route_table.route_table_private.id}"
-}
-resource "aws_route_table_association" "route_private_b" {
-  subnet_id      = "${aws_subnet.subnet_private_b.id}"
-  route_table_id = "${aws_route_table.route_table_private.id}"
 }
