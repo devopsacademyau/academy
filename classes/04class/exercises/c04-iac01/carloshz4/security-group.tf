68604b609c707220cf3466aec43157702f7de142
Carlos Zambrano <carlosh4@yahoo.com>
2020-07-24 13:07:13 +1000

    carloshz4/c04-iac01

--- classes/04class/exercises/c04-iac01/carloshz4/security-group.tf
+++ classes/04class/exercises/c04-iac01/carloshz4/security-group.tf
@@ -7,6 +7,15 @@ resource "aws_security_group" "da-sg" {
     to_port     = 22
     protocol    = "tcp"
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   tags = {
     Name = "devopsacademy-sg"
   }
