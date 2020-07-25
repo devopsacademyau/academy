20f05dadcb273bfd37136abc8eb564e423c869dd
jayanath <jayanath@gmail.com>
2020-07-25 11:45:12 +1000

    Add Terraform code for EC2 and S3 tasks

---
+++ classes/04class/exercises/c04-iac01/jayanath/security-group.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "sg_allow_ssh" {
+  name        = "sg_allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_allow_ssh"
+  }
+}
