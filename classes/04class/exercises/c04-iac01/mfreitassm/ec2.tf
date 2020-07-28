08ff38f7f36e4204e3d39aa7faaac563b9408da1
Mariana Freitas <mfreitas.sm@gmail.com>
2020-07-28 14:43:56 +1000

    edit files

--- classes/04class/exercises/c04-iac01/mfreitassm/ec2.tf
+++ classes/04class/exercises/c04-iac01/mfreitassm/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "devopsc04_ec2" {
   ami                  = "ami-0a58e22c727337c51"
   instance_type        = "t2.micro"
-  security_groups      = ["${aws_security_group.devops_c04_sg.name}"]
-  iam_instance_profile = "${aws_iam_instance_profile.devopsc04_profile.name}"
+  security_groups      = [aws_security_group.devops_c04_sg.name]
+  iam_instance_profile = aws_iam_instance_profile.devopsc04_profile.name
   key_name             = "DevopsAcademy"
 
   tags = {
--- classes/04class/exercises/c04-iac01/mfreitassm/iam.tf
+++ classes/04class/exercises/c04-iac01/mfreitassm/iam.tf
@@ -20,12 +20,12 @@ EOF
 
 resource "aws_iam_instance_profile" "devopsc04_profile" {
   name = "s3_readonly_profile"
-  role = "${aws_iam_role.devopsc04_role.name}"
+  role = aws_iam_role.devopsc04_role.name
 }
 
 resource "aws_iam_role_policy" "devopsc04_policy" {
   name = "s3_readonly_policy"
-  role = "${aws_iam_role.devopsc04_role.id}"
+  role = aws_iam_role.devopsc04_role.id
 
   policy = <<EOF
 {
