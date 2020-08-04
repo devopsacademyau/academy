79e8b92db7dc54d219b6c7452e47b383f1328e61
Gustavo Avelar <gpavelardev@gmail.com>
2020-08-04 21:58:17 +1000

    Remove comments

--- classes/04class/exercises/c04-iac03/gpavelar/variables.tf
+++ classes/04class/exercises/c04-iac03/gpavelar/variables.tf
@@ -8,27 +8,22 @@ variable "vpc_name" {
 
 variable "subnets_cidrs_public" {
   description = "CIDRs for public subnet"
-  # default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "subnets_names_public" {
   description = "Names for public subnets"
-  # default     = ["Public Subnet 3", "Public Subnet 4"]
 }
 
 variable "subnets_cidrs_private" {
   description = "CIDRs for private subnet"
-  # default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "subnets_names_private" {
   description = "Names for private subnets"
-  # default     = ["Private Subnet 1", "Private Subnet 2"]
 }
 
 variable "azs" {
   description = "Avaialbilities zones"
-  # default     = ["ap-southeast-2a", "ap-southeast-2b"]
 }
 
 variable "region" {
