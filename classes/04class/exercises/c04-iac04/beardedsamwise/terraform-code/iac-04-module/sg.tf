965a541fcaeb5689553d8bafb55a3c70d2fd49c4
Sam Bentley <sam.bentley@contino.io>
2022-02-14 09:17:09 +1100

    final updates to TF code

--- classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/iac-04-module/sg.tf
+++ classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/iac-04-module/sg.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "sg_web" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
 
   }
 
--- classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/iac-04-module/variables.tf
+++ classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/iac-04-module/variables.tf
@@ -1,4 +1,6 @@
 # Input variable definitions
+
+# Define two subnets in the 10.0.0.0/16 network
 variable "subnet1_cidr" {
   description = "CIDR of the first subnet."
   type        = string
@@ -7,10 +9,14 @@ variable "subnet2_cidr" {
   description = "CIDR of the second subnet."
   type        = string
 }
+
+# Define the EC2 AMI - Preferably Ubuntu or similar due to user data scripts
 variable "ec2_ami" {
   description = "Ubuntu AMI ID for EC2 Instance (or similar)"
   type        = string
 }
+
+# Define high and low CPU watermarks for auto scaling policy
 variable "high_cpu_threshold" {
   description = "Auto scaling high CPU threshold"
   type        = string
