b79de79e73c6a0ef010af34244dc274fb872b6e3
Sam Bentley <sam.bentley@contino.io>
2022-02-17 15:44:50 +1100

    fix missing threshold vars

--- classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/iac-04-module/ec2.tf
+++ classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/iac-04-module/ec2.tf
@@ -62,7 +62,7 @@ resource "aws_cloudwatch_metric_alarm" "highalarm" {
   namespace = "AWS/EC2"
   period = "120"
   statistic = "Average"
-  threshold = "70"
+  threshold = var.low_cpu_threshold
   dimensions = {
   "AutoScalingGroupName" = aws_autoscaling_group.web.name
   }
@@ -89,7 +89,7 @@ resource "aws_cloudwatch_metric_alarm" "lowalarm" {
   namespace = "AWS/EC2"
   period = "120"
   statistic = "Average"
-  threshold = "10"
+  threshold = var.high_cpu_threshold
   dimensions = {
   "AutoScalingGroupName" = aws_autoscaling_group.web.name
   }
--- classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/main.tfvars
+++ classes/04class/exercises/c04-iac04/beardedsamwise/terraform-code/main.tfvars
@@ -1,6 +1,6 @@
 # define variable values for deployment
-subnet1_cidr = "10.0.0.0/24"
-subnet2_cidr = "10.0.1.0/24"
-ec2_ami      = "ami-0b7dcd6e6fd797935"
+subnet1_cidr       = "10.0.0.0/24"
+subnet2_cidr       = "10.0.1.0/24"
+ec2_ami            = "ami-0b7dcd6e6fd797935"
 high_cpu_threshold = "80"
-low_cpu_threshold = "20"
+low_cpu_threshold  = "20"
