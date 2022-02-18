eabc313798ca1de9162c997724ce39b4e13559e3
Ravi Senevirathne <ravi.senevirathne@contino.io@Ravis-MacBook-Pro.local>
2022-02-18 13:08:01 +1100

    unwated comments have been removed on iam.tf

--- classes/04class/exercises/c04-iac01/ravisenevirathne/iam.tf
+++ classes/04class/exercises/c04-iac01/ravisenevirathne/iam.tf
@@ -26,32 +26,3 @@ data "aws_iam_policy_document" "iam_policy_doc"{
   }
 
 }
-
-
-/*
-resource "aws_iam_policy" "iam_policy" {
-  name = "iam_policy"
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:ListBucket"
-        ],
-        "Resource": "${aws_s3_bucket.S3.arn}"
-      }
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:GetObject",
-          "s3:PutObject"
-        ],
-        "Resource": "${aws_s3_bucket.S3.arn}/*"
-      }
-    ]
-  }
-EOF
-}
-*/
