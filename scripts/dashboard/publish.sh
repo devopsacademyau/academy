e8f982ddfe586ee5f988881245d492c0aca43220
Kiko <fcollet.silva@gmail.com>
2022-02-21 16:01:33 +1100

    Dash (#1703)
    * Adjust dashboard
    
    * Adjust dashboard

--- scripts/dashboard/publish.sh
+++ scripts/dashboard/publish.sh
@@ -5,13 +5,13 @@ export CHART_DATE=$(date +%Y-%m-%d)
 echo "Saving files to S3"
 aws s3 cp ./chart.png s3://devopsacademy.com.au/progression-chart/chart-${CHART_DATE}.png
 
-echo "Preparing the json payload"
-sed -i "s,PUBLISH_URL,$PUBLISH_URL,g" slack_payload.json
-sed -i "s,SLACK_CHANNEL,$SLACK_CHANNEL,g" slack_payload.json
-sed -i "s,CHART_DATE,$CHART_DATE,g" slack_payload.json
-sed -i "s,PUBLISH_URL,$PUBLISH_URL,g"  slack_payload.json
+# echo "Preparing the json payload"
+# sed -i "s,PUBLISH_URL,$PUBLISH_URL,g" slack_payload.json
+# sed -i "s,SLACK_CHANNEL,$SLACK_CHANNEL,g" slack_payload.json
+# sed -i "s,CHART_DATE,$CHART_DATE,g" slack_payload.json
+# sed -i "s,PUBLISH_URL,$PUBLISH_URL,g"  slack_payload.json
 
-echo "Publishing Slack message"
-curl -X POST -H 'Content-type: application/json' \
-  ${SLACK_WEBHOOK} \
-  -d @slack_payload.json
+# echo "Publishing Slack message"
+# curl -X POST -H 'Content-type: application/json' \
+#   ${SLACK_WEBHOOK} \
+#   -d @slack_payload.json
