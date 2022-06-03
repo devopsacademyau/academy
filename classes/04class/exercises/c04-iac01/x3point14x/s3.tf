4226564dee173108ea33980191112cc3e05e8482
Ben Currey <x3point14x@gmail.com>
2022-06-03 11:19:05 +1000

    Renamed s3 bucket to meet aws standards

--- classes/04class/exercises/c04-iac01/x3point14x/s3.tf
+++ classes/04class/exercises/c04-iac01/x3point14x/s3.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/aws/4.16.0/docs/resources/s3_bucket
 resource "aws_s3_bucket" "bcurrey_iac_1_s3" {
-    bucket = "bcurrey_iac_1_s3"
+    bucket = "bcurrey-iac-1-s3"
 }
 # https://registry.terraform.io/providers/hashicorp/aws/4.16.0/docs/resources/s3_bucket_acl
 resource "aws_s3_bucket_acl" "bcurrey_iac_1_s3_acl" {
