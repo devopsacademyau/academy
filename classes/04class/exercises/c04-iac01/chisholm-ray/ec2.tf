d319560fe39fe4755e64c6464c54ac2c38770029
Julio Pimentel <julio.pimentelalbores@contino.io>
2022-08-16 10:17:02 +1000

    Revert "answers and terraform files for c04-iac01 (#1596)"
    This reverts commit 255da4aa4d5b777a286198583bc96c092ae1ddea.

--- classes/04class/exercises/c04-iac01/chisholm-ray/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "this" {
-  ami                    = "ami-042c4533fa25c105a"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ssh.id]
-  key_name               = "conorcr-kp"
-  iam_instance_profile = aws_iam_instance_profile.this.name
-}
-
