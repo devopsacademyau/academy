2b4e422faecd6280cf2c429d8f9afeb34276fd9f
William Chrisp <william.chrisp@contino.io>
2022-08-25 10:00:47 +1000

    Add answer for c04-iac02

---
+++ classes/04class/exercises/c04-iac02/williamchrisp/README.md
@@ -0,0 +1,15 @@
+# C04-IAC02
+
+## Terraform code 
+- [_backend.tf](_backend.tf)
+- [_provider.tf](_provider.tf)
+- [vpc.tf](vpc.tf)
+- [output.tf](output.tf)
+- [variables.tf](variables.tf)
+- [main.tfvars](main.tfvars)
+
+## Command Execution Output
+- [output.txt](output.txt)
+ 
+***
+Answer for exercise [c04-iac02](https://github.com/devopsacademyau/academy/blob/c41e824fb2a2c55e3a30b2371a87e3a7551b6741/classes/04class/exercises/c04-iac02/README.md)
---
+++ classes/04class/exercises/c04-iac02/williamchrisp/variables.tf
@@ -0,0 +1,28 @@
+# Variables
+variable "vpc_cidr" {
+  type    = string
+}
+variable "subnet1_private_name" {
+  type    = string
+}
+variable "subnet1_private_cidr" {
+  type    = string
+}
+variable "subnet2_private_name" {
+  type    = string
+}
+variable "subnet2_private_cidr" {
+  type    = string
+}
+variable "subnet1_public_name" {
+  type    = string
+}
+variable "subnet1_public_cidr" {
+  type    = string
+}
+variable "subnet2_public_name" {
+  type    = string
+}
+variable "subnet2_public_cidr" {
+  type    = string
+}
