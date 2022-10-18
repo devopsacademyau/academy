f84e2b26cde2a9d073a4a36c177e7bfbafd98cce
Matt Garces <admdh78@bendigoadelaide.com.au>
2022-10-18 22:25:00 +1100

    fix(ci): Rework workflow

--- scripts/secret_check.sh
+++ scripts/secret_check.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo '----Checking if File exists----'
+test=../classes/01class/exercises/c01-git04/starkmatt/c01-git04.txt
+echo '----Checking if secret exists----'
+
+verify=$(awk '/SECRET=/{print $1}' "$test")
+
+if [ -n "$verify" ];
+then
+    echo "Secret Exists!"
+else
+    echo "Secret does not exist"
+fi
