824a92c0e506633a7bcbf29d1a74e2f38be0d1c1
Caio Trevisan <caiocezar.t@gmail.com>
2022-11-05 00:55:31 -0300

    test review exercise

--- reviewer/exercises/validate.py
+++ reviewer/exercises/validate.py
@@ -1,4 +1,16 @@
-import os
+import json
 import sys
 
-print(sys.argv[1])
+files_changed = sys.argv[1].split(',')
+exercises_changed = []
+
+for f in files_changed:
+    exercise = f.split('/')[2]
+    if not exercise in exercises_changed:
+        exercises_changed.append(exercise)
+
+if len(exercises_changed) > 1:
+    print("multiple exercises changed, please amend your PR")
+    sys.exit()
+
+print(exercises_changed[0])
