d255c32e0ca5c7a088244c40bcc1035b7d9c801a
Caio Trevisan <caiocezar.t@gmail.com>
2022-11-05 18:26:24 -0300

    test review exercise

--- reviewer/exercises/git04/review.py
+++ reviewer/exercises/git04/review.py
@@ -3,7 +3,5 @@ import sys
 secret_file = "my_env.txt"
 my_env = open("/exercise/{}".format(secret_file))
 if my_env:
-    print("Secret file still exists")
+    print("fail")
     sys.exit()
-
-print("{} file not found. Well done cleaning all your secrets.".format(secret_file))
