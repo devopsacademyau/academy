b8b1e64266e41f7f5584911181b4e641081cccf2
Caio Trevisan <caiocezar.t@gmail.com>
2022-11-07 00:44:57 -0300

    test exercise reviewer

--- reviewer/exercises/git04/review.py
+++ reviewer/exercises/git04/review.py
@@ -7,7 +7,6 @@ try:
 
 except:
     print("Secret file {} not found. Well done cleaning all your secrets.".format(secret_file))
-    print("Secret file {} not found. Well done cleaning all your secrets. >> $GITHUB_STEP_SUMMARY".format(secret_file))
     sys.exit()
 
 print("::error title='Wrong answer'::'Secret file {} still exists. Please update your code and submit again.'".format(secret_file))
