5a4cd31a381afa9e970286926fe7248a992ea5f3
Caio Trevisan <caiocezar.t@gmail.com>
2022-11-07 15:43:16 +1100

    test exercise reviewer (#1959)

--- reviewer/exercises/git04/review.py
+++ reviewer/exercises/git04/review.py
@@ -1,10 +1,21 @@
 import sys
 
+def github_error(message):
+    print("::error title=Exercise submission failed::{}".format(message))
+
+def github_summary(message):
+    print("SUMMARY: {}".format(message))
+
+
 secret_file = "my_env.txt"
+
 try:
     my_env = open("/exercise/{}".format(secret_file))
-    print("fail")
-    sys.exit()
 
 except:
-    print("done")
+    github_summary("### Exercise completed :rocket:")
+    github_summary("Secret file {} not found. Well done cleaning all your secrets.".format(secret_file))
+    sys.exit()
+
+github_error("Secret file {} still exists. Please update your code and submit again.".format(secret_file))
+raise Exception("Submission failed.")
