0f27df94cc629172b63c1edbddb0cf51114d35cf
Caio Trevisan <caiocezar.t@gmail.com>
2022-11-07 00:30:59 -0300

    test exercise reviewer

--- reviewer/exercises/git04/review.py
+++ reviewer/exercises/git04/review.py
@@ -1,11 +1,13 @@
 import sys
 
 secret_file = "my_env.txt"
+fail = False
 try:
     my_env = open("/exercise/{}".format(secret_file))
-    print("::error title='Wrong answer'::'Secret file still exists. Please update your code and submit again.'")
-    print("exit 1")
-    sys.exit()
 
 except:
-    print("done")
+    print("Secret file {} not found. Well done cleaning all your secrets.".format(secret_file))
+    sys.exit()
+
+::error title='Wrong answer'::'Secret file {} still exists. Please update your code and submit again.'".format(secret_file)
+raise Exception("Submission failed.")
